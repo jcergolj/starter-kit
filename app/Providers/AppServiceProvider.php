ab9adf32ffc0c6567421dabaf0586cd56d6318ad
Janez Cergolj <me@jcergolj.me.uk>
2026-03-17 12:39:14 +0100

    add brevo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,11 @@ namespace App\Providers;
 use App\Enums\RoleEnum;
 use App\Services\TenantDatabaseService;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 use Opcodes\LogViewer\Facades\LogViewer;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
 use URL;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Mail::extend('brevo', fn () => (new BrevoTransportFactory)->create(
+            new Dsn(
+                'brevo+api',
+                'default',
+                config('services.brevo.key')
+            )
+        ));
+
         LogViewer::auth(fn ($request) => $request->user()?->role === RoleEnum::Superadmin);
 
         DB::prohibitDestructiveCommands($this->app->isProduction());
